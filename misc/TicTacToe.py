0b170c1146ab1cee8788a26bfbd3cdc21d16b498
OhmReal <poiu80727@gmail.com>
2025-09-19 15:02:21 +0700

    added win checking and placing checking

--- misc/TicTacToe.py
+++ misc/TicTacToe.py
@@ -2,20 +2,45 @@ import numpy as n
 count = 0
 arr = n.array([1,2,3,4,5,6,7,8,9],dtype='str')
 table = n.reshape(arr,[3,3])
-def chec(inp):
+win = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{6,4,8}]
+OSel = []
+XSel = []
+
+def place(inp):
     global count
     if count % 2 == 0:
-        arr[inp] = "O"
-    else:
-        arr[inp] = "X"
+        if arr[inp] != "X":
+            arr[inp] = "O"
+            OSel.append(inp)
+        else:
+            count -= 1
+    if count % 2 == 1:
+        if arr[inp] != "O":
+            arr[inp] = "X"
+            XSel.append(inp)
+        else:
+            count -= 1 
     print(table)
+
+def check():
+    Hold1 = set(OSel)
+    Hold2 = set(XSel)
+    if Hold1 in win:
+        print("O has won the game")
+        return True
+    if Hold2 in win:
+        print("X has won the game")
+        return True
 
 def inp():
     global count
     while count<9:
+        if check() == True:
+            break
         inp =( int(input('Enter :'))-1)
-        chec(inp)
+        place(inp)
         count += 1
-
+        
+        
 print("Enter number you want to change to X/O",'\n',table)
 inp()
